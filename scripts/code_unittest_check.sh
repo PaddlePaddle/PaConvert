6ac5007f3fabb2a6c992f790d7a418e0ac71d711
wangfengsheng1999 <wangfengsheng1999@gmail.com>
2023-05-12 11:14:40 +0000

    code inconsistency ci

--- scripts/code_unittest_check.sh
+++ scripts/code_unittest_check.sh
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -x
+
 # pre-commit multi-thread running.
 echo "Checking code unit test by pytest ..."
 /root/anaconda3/bin/pytest /workspace/$1/PaConvert/tests;check_error=$?
