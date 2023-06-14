15afbb118d3a54caa970c644941b981494c870fa
Fengsheng Wang <wangfengsheng1999@gmail.com>
2023-06-13 20:34:50 -0700

    [CI] log paddle commit and version (#98)
    * add fix api
    
    * fix bug
    
    * fix bug
    
    * add ci

--- scripts/code_modeltest_check.sh
+++ scripts/code_modeltest_check.sh
@@ -20,6 +20,10 @@ DEVELOP_IF="OFF"
 if [[ "$DEVELOP_IF" == "OFF" ]]; then
     cd /workspace/$2/PaConvert/
     PATH=$1
+    echo "Insalling develop version paddle"
+    python -m pip install --no-cache-dir  --force-reinstall paddlepaddle==0.0.0 -f https://www.paddlepaddle.org.cn/whl/linux/cpu-mkl/develop.html
+    python -c "import paddle; print('paddle version information:' ,paddle.__version__); print('paddle commit information:' ,paddle.__git_commit__)"
+
 fi
 
 echo '*******************************start modeltest test*********************************'
