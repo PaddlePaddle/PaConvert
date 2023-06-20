77485fd4e93fcb8cac84ac294b040aa5dcaf15cb
LokeZhou <aishenghuoaiqq@163.com>
2023-06-20 12:41:14 +0000

    fix mseloss

--- paconvert/api_matcher.py
+++ paconvert/api_matcher.py
@@ -3589,55 +3589,6 @@ class LossMatcher(BaseMatcher):
         return code
 
 
-class MseLossMatcher(BaseMatcher):
-    def generate_code(self, kwargs):
-        if "size_average" in kwargs:
-            size_average = kwargs.pop("size_average")
-            if "True" in size_average:
-                size_average = True
-            elif "False" in size_average:
-                size_average = False
-            else:
-                size_average = None
-        else:
-            size_average = None
-
-        if "reduce" in kwargs:
-            reduce = kwargs.pop("reduce")
-            if "True" in reduce:
-                reduce = True
-            elif "False" in reduce:
-                reduce = False
-            else:
-                reduce = None
-        else:
-            reduce = None
-
-        if size_average is not None or reduce is not None:
-            if size_average is None:
-                size_average = True
-            if reduce is None:
-                reduce = True
-
-            if size_average and reduce:
-                reduction = '"""mean"""'
-            elif reduce:
-                reduction = '"""sum"""'
-            else:
-                reduction = '"""none"""'
-
-            kwargs["reduction"] = reduction
-
-        API_TEMPLATE = textwrap.dedent(
-            """
-            paddle.nn.MSELoss({})
-            """
-        )
-        code = API_TEMPLATE.format(self.kwargs_to_str(kwargs))
-
-        return code
-
-
 class TupleAssignMatcher(BaseMatcher):
     def generate_code(self, kwargs):
         kwargs_change = {}
@@ -3817,6 +3768,7 @@ class FunctionalUnfoldMatcher(BaseMatcher):
         code = API_TEMPLACE.format(self.kwargs_to_str(kwargs))
 
         return code
+
 
 class ParameterMatcher(BaseMatcher):
     def get_paddle_nodes(self, args, kwargs):
@@ -4038,4 +3990,3 @@ class SoftmaxMatcher(BaseMatcher):
         if "dim" not in kwargs:
             return None
         return GenericMatcher.generate_code(self, kwargs)
-
