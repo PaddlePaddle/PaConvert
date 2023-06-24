2a9a92fa00728a1e71e11cd6bc1cf6df7ed23ba5
Liyulingyue <852433440@qq.com>
2023-06-24 20:25:23 +0800

    torch.nn.functional.poisson_nll_loss

--- tests/test_nn_functional_poisson_nll_loss.py
+++ tests/test_nn_functional_poisson_nll_loss.py
@@ -48,7 +48,7 @@ def test_case_2():
     obj.run(pytorch_code, ["x1", "x2", "result"])
 
 
-def test_case_3():
+def _test_case_3():
     pytorch_code = textwrap.dedent(
         """
         import torch
