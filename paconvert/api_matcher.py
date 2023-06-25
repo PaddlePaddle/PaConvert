5bc36b367e532118afe9eaa8174f932979a39a81
LokeZhou <aishenghuoaiqq@163.com>
2023-06-25 03:17:58 +0000

    UtilsCppExtensionMatcher bug

--- paconvert/api_matcher.py
+++ paconvert/api_matcher.py
@@ -3649,7 +3649,7 @@ class SizeAverageMatcher(BaseMatcher):
 
 class UtilsCppExtensionMatcher(BaseMatcher):
     def generate_code(self, kwargs):
-        if "name" in list(kwargs.leys()):
+        if "name" in list(kwargs.keys()):
             kwargs.pop("name")
         code = "{}({})".format(self.get_paddle_api(), self.kwargs_to_str(kwargs))
         return code
