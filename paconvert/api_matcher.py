ac7c8e51e022b29d8b6560afc3d2572e3cf00a66
zh-hike <1583124882@qq.com>
2023-07-11 12:19:39 +0000

    Add APIs of different types of Tensor
    * torch.cuda.IntTensor
    
    * torch.cuda.FloatTensor
    
    * torch.cuda.LongTensor
    
    * torch.BoolTensor
    
    * torch.cuda.BoolTensor
    
    * torch.ByteTensor
    
    * torch.cuda.ByteTensor

--- paconvert/api_matcher.py
+++ paconvert/api_matcher.py
@@ -512,12 +512,31 @@ class TensorMatcher(BaseMatcher):
                 shape = astor.to_source(args[0].value).strip("\n")
             else:
                 data = self.parse_args(args)[0]
-                if "torch.IntTensor" == self.torch_api:
+                if (
+                    "torch.IntTensor" == self.torch_api
+                    or "torch.cuda.IntTensor" == self.torch_api
+                ):
                     code = "paddle.to_tensor(data={}, dtype='int32')".format(data)
-                elif "torch.LongTensor" == self.torch_api:
+                elif (
+                    "torch.LongTensor" == self.torch_api
+                    or "torch.cuda.LongTensor" == self.torch_api
+                ):
                     code = "paddle.to_tensor(data={}, dtype='int64')".format(data)
-                elif "torch.FloatTensor" == self.torch_api:
+                elif (
+                    "torch.FloatTensor" == self.torch_api
+                    or "torch.cuda.FloatTensor" == self.torch_api
+                ):
                     code = "paddle.to_tensor(data={}, dtype='float32')".format(data)
+                elif (
+                    "torch.ByteTensor" == self.torch_api
+                    or "torch.cuda.ByteTensor" == self.torch_api
+                ):
+                    code = "paddle.to_tensor(data={}, dtype='uint8')".format(data)
+                elif ("torch.BoolTensor" == self.torch_api) or (
+                    "torch.cuda.BoolTensor" == self.torch_api
+                ):
+                    code = "paddle.to_tensor(data={}).astype('bool')".format(data)
+
                 else:
                     if not isinstance(args[0], ast.Name):
                         code = "paddle.to_tensor(data={}, dtype='float32')".format(data)
@@ -527,12 +546,30 @@ class TensorMatcher(BaseMatcher):
                 return node
             shape = str(shape).replace("'", "")
 
-        if "torch.IntTensor" == self.torch_api:
+        if (
+            "torch.IntTensor" == self.torch_api
+            or "torch.cuda.IntTensor" == self.torch_api
+        ):
             code = "paddle.empty(shape={}, dtype='int32')".format(shape)
-        elif "torch.LongTensor" == self.torch_api:
+        elif (
+            "torch.LongTensor" == self.torch_api
+            or "torch.cuda.LongTensor" == self.torch_api
+        ):
             code = "paddle.empty(shape={}, dtype='int64')".format(shape)
-        elif "torch.FloatTensor" == self.torch_api:
+        elif (
+            "torch.FloatTensor" == self.torch_api
+            or "torch.cuda.FloatTensor" == self.torch_api
+        ):
             code = "paddle.empty(shape={}, dtype='float32')".format(shape)
+        elif (
+            "torch.ByteTensor" == self.torch_api
+            or "torch.cuda.ByteTensor" == self.torch_api
+        ):
+            code = "paddle.zeros(shape={}, dtype='uint8')".format(shape)
+        elif ("torch.BoolTensor" == self.torch_api) or (
+            "torch.cuda.BoolTensor" == self.torch_api
+        ):
+            code = "paddle.randint(0, 2, shape={}).astype('bool')".format(shape)
         else:
             code = "paddle.empty(shape={})".format(shape)
 
---
+++ tests/test_cuda_BoolTensor.py
@@ -0,0 +1,106 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import textwrap
+
+from apibase import APIBase
+
+obj = APIBase("torch.cuda.BoolTensor")
+
+
+def _test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.cuda.BoolTensor(2, 3)
+        """
+    )
+    obj.run(pytorch_code, ["result"], check_value=False)
+
+
+def _test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        shape = [2, 3]
+        result = torch.cuda.BoolTensor(*shape)
+        """
+    )
+    obj.run(pytorch_code, ["result"], check_value=False)
+
+
+def _test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        dim1, dim2 = 2, 3
+        result = torch.cuda.BoolTensor(dim1, dim2)
+        """
+    )
+    obj.run(pytorch_code, ["result"], check_value=False)
+
+
+def _test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        def fun(x: torch.cuda.BoolTensor):
+            return x * 2
+
+        a = torch.cuda.BoolTensor(3, 4)
+        result = fun(a)
+        """
+    )
+    obj.run(
+        pytorch_code,
+        ["result"],
+        check_value=False,
+        unsupport=True,
+        reason="paddle does not support BoolTensor types and int multiplication",
+    )
+
+
+def _test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.cuda.BoolTensor([[3, 4], [5, 8]])
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def _test_case_6():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.cuda.BoolTensor((1, 2, 3))
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def _test_case_7():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.cuda.BoolTensor()
+        """
+    )
+    obj.run(
+        pytorch_code,
+        ["result"],
+        unsupport=True,
+        reason="paddle does not support 0-Size Tensor",
+    )
