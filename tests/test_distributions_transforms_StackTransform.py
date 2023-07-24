6eac42df62182f41f239e073f373c6d1e026672b
co63oc <co63oc@users.noreply.github.com>
2023-07-24 14:13:10 +0800

    转换规则 No. 193/194/195 (#182)
    Add tests

---
+++ tests/test_distributions_transforms_StackTransform.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import textwrap
+
+from apibase import APIBase
+
+obj = APIBase("torch.distributions.transforms.StackTransform")
+
+
+def test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones((1, 2))
+        tseq = [torch.distributions.transforms.SoftmaxTransform()]
+        t = torch.distributions.transforms.StackTransform(tseq)
+        result = t(x)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones((1, 2))
+        tseq = [torch.distributions.transforms.SoftmaxTransform()]
+        t = torch.distributions.transforms.StackTransform(tseq, cache_size=0)
+        result = t(x)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones((2, 1))
+        tseq = [torch.distributions.transforms.SoftmaxTransform()]
+        t = torch.distributions.transforms.StackTransform(tseq, dim=1)
+        result = t(x)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
