2060c0649a4b79cac35d2ca7c5c0e156ca6aad3f
zhengqiwen1997 <zhengqiwen_au@163.com>
2023-08-03 11:22:40 +0000

    add torch.optim.Adagrad

--- tests/test_optim_Optimizer_load_state_dict.py
+++ tests/test_optim_Optimizer_load_state_dict.py
@@ -75,3 +75,49 @@ def test_case_2():
         unsupport=True,
         reason="currently not support optimizer subclass API",
     )
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        import torch.nn as nn
+
+        theta = torch.tensor([1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0], requires_grad=True)
+        l = torch.nn.Linear(10, 1)
+        optim = torch.optim.Adadelta(l.parameters(), lr = 1.0)
+        z = l(theta)
+        z.backward()
+        optim.step()
+        result = optim.state_dict()
+        optim.load_state_dict(result)
+        result = optim.state_dict()
+        """
+    )
+    obj.run(
+        pytorch_code,
+        ["result"],
+    )
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        import torch.nn as nn
+
+        theta = torch.tensor([1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0], requires_grad=True)
+        l = torch.nn.Linear(10, 1)
+        optim = torch.optim.Adagrad(l.parameters(), lr = 1.0)
+        z = l(theta)
+        z.backward()
+        optim.step()
+        result = optim.state_dict()
+        optim.load_state_dict(result)
+        result = optim.state_dict()
+        """
+    )
+    obj.run(
+        pytorch_code,
+        ["result"],
+    )
