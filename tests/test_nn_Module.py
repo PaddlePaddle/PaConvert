6a17758f99c90ac79c5f19f3b6feb3cc7df7ae41
shuaills <shishuaiuoe@gmail.com>
2023-08-03 21:06:25 +0800

    test torch.nn.Module()

---
+++ tests/test_nn_Module.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+
+import textwrap
+
+from apibase import APIBase
+
+obj = APIBase("torch.nn.Module")
+
+def test_case_1():
+    # 初始化
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        module1 = torch.nn.Module()
+        result = None
+
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+    
+def test_case_2():
+    # 使用自定义参数初始化
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        module1 = torch.nn.Module(custom_arg=10)
+        result = None
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+    
+def test_case_3():
+    # 调用类内函数
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        module1 = torch.nn.Module()
+        module1.custom_func()
+        result = None
+        """
+    )
+    obj.run(pytorch_code, ["result"])
