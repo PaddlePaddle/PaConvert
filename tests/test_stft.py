e678380b36d6edd3413d41360e0ce69789671325
enkilee <luhputu0815@gmail.com>
2023-08-08 08:44:53 +0800

    add unittest

--- tests/test_stft.py
+++ tests/test_stft.py
@@ -116,8 +116,9 @@ def test_case_6():
                             [ 0.4907, -1.3948, -1.0691, -0.3132],
                             [-1.6092,  0.5419, -0.2993,  0.3195]])
         n_fft = 4
-        window = torch.hann_window(4)
-        result = torch.stft(x, n_fft=n_fft, center=False, window=window, return_complex=True)
+        hop_length = 4
+        win_length = 4
+        result = torch.stft(x, n_fft=n_fft, center=False, hop_length=hop_length, win_length=win_length, return_complex=True)
         """
     )
     obj.run(pytorch_code, ["result"])
@@ -147,7 +148,24 @@ def test_case_8():
                             [ 0.4907, -1.3948, -1.0691, -0.3132],
                             [-1.6092,  0.5419, -0.2993,  0.3195]])
         n_fft = 4
-        result = torch.stft(x, n_fft=n_fft, center=False, onsided=False, return_complex=True)
+        hop_length = 4
+        result = torch.stft(x, n_fft=n_fft, center=False, hop_length=hop_length, normalized=True, return_complex=True)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_9():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.tensor([[ 1.3398,  0.2663, -0.2686,  0.2450],
+                            [-0.7401, -0.8805, -0.3402, -1.1936],
+                            [ 0.4907, -1.3948, -1.0691, -0.3132],
+                            [-1.6092,  0.5419, -0.2993,  0.3195]])
+        n_fft = 4
+        win_length = 4
+        result = torch.stft(x, n_fft=n_fft, center=False, win_length=win_length, normalized=True, return_complex=True)
         """
     )
     obj.run(pytorch_code, ["result"])
