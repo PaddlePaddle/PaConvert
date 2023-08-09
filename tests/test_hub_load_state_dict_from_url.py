e7ae7cfe0a93dbfeb7a3d9e02f250aac15881b84
LokeZhou <aishenghuoaiqq@163.com>
2023-08-09 08:43:11 +0000

    update unit test

--- tests/test_hub_load_state_dict_from_url.py
+++ tests/test_hub_load_state_dict_from_url.py
@@ -40,7 +40,7 @@ def test_case_2():
     obj.run(pytorch_code, ["result"])
 
 
-def test_alias_case_1():
+def _test_alias_case_1():
     pytorch_code = textwrap.dedent(
         """
         import torch
