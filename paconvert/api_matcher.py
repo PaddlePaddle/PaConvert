097055b574321104797686d72510796a27e96969
zhwesky2010 <zhouwei25@baidu.com>
2023-08-22 10:38:17 +0000

    fix some bug

--- paconvert/api_matcher.py
+++ paconvert/api_matcher.py
@@ -3853,12 +3853,11 @@ class SvdMatcher(BaseMatcher):
         out_v = kwargs.pop("out") if "out" in kwargs else None
         some_v = kwargs.pop("some") if "some" in kwargs else None
 
+        kwargs["x"] = kwargs.pop("input")
         if some_v:
             kwargs["full_matrices"] = "not " + some_v.strip("()")
 
-        kwargs["x"] = kwargs.pop("input")
         if out_v:
-
             API_TEMPLATE = textwrap.dedent(
                 """
                 tmp_u, tmp_s, tmp_v = {}({})
@@ -3872,22 +3871,17 @@ class SvdMatcher(BaseMatcher):
                 out_v,
                 out_v,
             )
-
-            return code
-
-        API_TEMPLATE = textwrap.dedent(
-            """
-            tmp_u, tmp_s, tmp_v = {}({})
-            tmp_u, tmp_s, tmp_v.conj().t()
-            """
-        )
-        code = API_TEMPLATE.format(
-            self.get_paddle_api(),
-            self.kwargs_to_str(kwargs),
-            out_v,
-            out_v,
-            out_v,
-        )
+        else:
+            API_TEMPLATE = textwrap.dedent(
+                """
+                tmp_u, tmp_s, tmp_v = {}({})
+                tmp_u, tmp_s, tmp_v.conj().t()
+                """
+            )
+            code = API_TEMPLATE.format(
+                self.get_paddle_api(),
+                self.kwargs_to_str(kwargs),
+            )
 
         return code
 
