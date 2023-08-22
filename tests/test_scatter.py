7ab248578979af7058e5355f3e3909db78143337
LokeZhou <aishenghuoaiqq@163.com>
2023-08-22 17:40:52 +0800

    torch.scatter add out args (#238)

--- tests/test_scatter.py
+++ tests/test_scatter.py
@@ -64,8 +64,34 @@ def test_case_4():
         """
         import torch
         index = torch.tensor([[0],[1],[2]])
-        input = torch.zeros(3, 5)
+        input = torch.ones(3, 5)
         result = torch.scatter(input,1, index, 1.0,reduce='add')
         """
     )
     obj.run(pytorch_code, ["result"])
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        index = torch.tensor([[0],[1],[2]])
+        input = torch.zeros(3, 5)
+        out = torch.zeros(3, 5)
+        result = torch.scatter(input,1, index, 1.0,reduce='add',out=out)
+        """
+    )
+    obj.run(pytorch_code, ["out"])
+
+
+def test_case_6():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        index = torch.tensor([[0],[1],[2]])
+        input = torch.zeros(3, 5)
+        out = torch.zeros(3, 5)
+        result = torch.scatter(input,1, index, 1.0,out=out)
+        """
+    )
+    obj.run(pytorch_code, ["out"])
