e66bc1b027b55a1b0d04b99c1de46a345a882228
zhengqiwen1997 <zhengqiwen_au@163.com>
2023-08-23 08:44:27 +0000

    AdamW test2

--- tests/test_optim_AdamW.py
+++ tests/test_optim_AdamW.py
@@ -100,7 +100,7 @@ def test_case_2():
         result = conv.weight.mean()
         """
     )
-    obj.run(pytorch_code, ["result"], rtol=1.0e-5)
+    obj.run(pytorch_code, ["result"], rtol=1.0e-4)
 
 
 def test_case_3():
