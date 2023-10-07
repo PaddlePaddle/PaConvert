fcbebf59fe828135beb487671640ef711831fad3
LoneRanger <836253168@qq.com>
2023-10-07 23:42:04 +0800

    Update test_cuda_get_device_name.py

--- tests/test_cuda_get_device_name.py
+++ tests/test_cuda_get_device_name.py
@@ -23,8 +23,9 @@ def test_case_1():
     pytorch_code = textwrap.dedent(
         """
         import torch
-        current_device = torch.cuda.current_device()
-        result = torch.cuda.get_device_name(current_device)
+        if torch.cuda.is_available():
+            current_device = torch.cuda.current_device()
+            result = torch.cuda.get_device_name(current_device)
         """
     )
     obj.run(
