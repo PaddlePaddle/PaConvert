b9af2565f02b17051c68303da4e449da1be52564
李芳钰 <56572498+Li-fAngyU@users.noreply.github.com>
2023-10-12 00:37:52 +0800

    Update test_Tensor_view.py

--- tests/test_Tensor_view.py
+++ tests/test_Tensor_view.py
@@ -30,48 +30,105 @@ def test_case_1():
     obj.run(pytorch_code, ["result"])
 
 
-test_case_1()
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(4.)
+        result = a.view((2, 2))
+        """
+    )
+    obj.run(pytorch_code, ["result"])
 
-# def test_case_2():
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(9.)
+        result = a.view([3, 3])
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(9)
+        shape = (3, 3)
+        result = a.view(shape)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(9)
+        shape = (3, 3)
+        result = a.view(*shape)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_6():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(4.)
+        k = 2
+        result = a.view((k, k))
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_7():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(2.)
+        k = 2
+        result = a.view(k)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+# # 因为当前paddle.view 不支持没有 shape 的 tensor，所以该案例无法正常运行
+# def test_case_9():
 #     pytorch_code = textwrap.dedent(
 #         """
 #         import torch
-#         a = torch.arange(9)
-#         shape = (3, 3)
-#         result = torch.Tensor.view(a, shape)
+#         a = torch.tensor(1.)
+#         result = a.view(1)
 #         """
 #     )
 #     obj.run(pytorch_code, ["result"])
 
 
-# def test_case_3():
-#     pytorch_code = textwrap.dedent(
-#         """
-#         import torch
-#         a = torch.arange(4.)
-#         k = 2
-#         result = torch.Tensor.view(a, (k, k))
-#         """
-#     )
-#     obj.run(pytorch_code, ["result"])
+def test_case_8():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(6.)
+        result = a.view((2, 3))
+        """
+    )
+    obj.run(pytorch_code, ["result"])
 
 
-# def test_case_4():
-#     pytorch_code = textwrap.dedent(
-#         """
-#         import torch
-#         a = torch.arange(24.)
-#         result = torch.Tensor.view(a, (-1, ))
-#         """
-#     )
-#     obj.run(pytorch_code, ["result"])
-
-
-# def test_case_5():
-#     pytorch_code = textwrap.dedent(
-#         """
-#         import torch
-#         result = torch.Tensor.view(input=torch.arange(4.), shape=(2, 2))
-#         """
-#     )
-#     obj.run(pytorch_code, ["result"])
+def test_case_9():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(6.)
+        result = a.view(torch.int32)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
