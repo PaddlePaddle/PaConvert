1f5b7b7dd42426a5dd7d0f67a5161d872a04b9cb
李芳钰 <56572498+Li-fAngyU@users.noreply.github.com>
2023-10-20 11:20:08 +0800

    【Hackathon 5th No.42】 为Paddle代码转换工具新增API转换规则 (第1组 编号1-20) (#306)
    * 增加torch.geqrf单侧。
    
    * first commit
    
    * Update test_Tensor_reshape.py
    
    * fix code style.
    
    * Update api_mapping.json
    
    * Update api_matcher.py
    
    * Update test_Tensor_T.py
    
    * Update test_Tensor_view.py
    
    * Update test_Tensor.py
    
    * Update api_mapping.json
    
    * Update api_matcher.py
    
    * update
    
    * update test_Tensor_view
    
    * Update api_matcher.py
    
    * update TensorViewMatcher in api_matcher.py
    
    * Update api_matcher.py

--- paconvert/api_matcher.py
+++ paconvert/api_matcher.py
@@ -2935,6 +2935,42 @@ class HypotMatcher(BaseMatcher):
         return code
 
 
+class TensorViewMatcher(BaseMatcher):
+    def generate_aux_code(self):
+        CODE_TEMPLATE = textwrap.dedent(
+            """
+            def view(self, *args, **kwargs):
+                if args:
+                    if len(args)==1 and isinstance(args[0], (tuple, list, str)):
+                        return paddle.view(self, args[0])
+                    else:
+                        return paddle.view(self, list(args))
+                elif kwargs:
+                    key = [k for k in kwargs.keys()]
+                    return paddle.view(self, shape_or_dtype = kwargs[key[0]])
+
+            setattr(paddle.Tensor, 'view', view)
+            """
+        )
+        return CODE_TEMPLATE
+
+    def get_paddle_class_nodes(self, func, args, kwargs):
+        if kwargs:
+            if len(kwargs) == 1:
+                self.write_aux_code()
+                return "unchange"
+
+        if args:
+            if len(args) == 1 and isinstance(args[0], (ast.Tuple, ast.List)):
+                return "unchange"
+            if isinstance(args[0], (ast.Constant)) and isinstance(args[0].value, str):
+                return "unchange"
+            self.write_aux_code()
+            return "unchange"
+
+        return "misidentify"
+
+
 class TensorReshapeMatcher(BaseMatcher):
     def generate_aux_code(self):
         CODE_TEMPLATE = textwrap.dedent(
---
+++ tests/test_Tensor_index_put.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import textwrap
+
+from apibase import APIBase
+
+obj = APIBase("torch.Tensor.index_put")
+
+
+def test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones([5, 3])
+        t = torch.tensor([1.], dtype=torch.float)
+        indices = [torch.tensor(i) for i in [[0, 0], [0, 1]]]
+        result = x.index_put(indices, t)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones([5, 3])
+        t = torch.tensor([1.], dtype=torch.float)
+        indices = [torch.tensor(i) for i in [[0, 0], [0, 1]]]
+        result = x.index_put(indices, values=t)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones([5, 3])
+        t = torch.tensor([1.], dtype=torch.float)
+        indices = [torch.tensor(i) for i in [[0, 0], [0, 1]]]
+        result = x.index_put(indices, t, True)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones([5, 3])
+        t = torch.tensor([1.], dtype=torch.float)
+        indices = [torch.tensor(i) for i in [[0, 0], [0, 1]]]
+        result = x.index_put(indices=indices, values=t, accumulate=True)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones([5, 3])
+        t = torch.tensor([1.], dtype=torch.float)
+        indices = [torch.tensor(i) for i in [[0, 0], [0, 1]]]
+        result = x.index_put(indices=indices, values=t, accumulate=False)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_6():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.ones([5, 3])
+        t = torch.tensor([1.], dtype=torch.float)
+        indices = [torch.tensor(i) for i in [[0, 0], [0, 1]]]
+        result = x.index_put(accumulate=True, indices=indices, values=t)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
