0c7e5f64bf43914adaa9560c27287bd232a2244a
RedContritio <RedContritio@qq.com>
2023-11-14 15:44:52 +0800

    fix 1 unittest error in Tensor.copy_

--- paconvert/api_matcher.py
+++ paconvert/api_matcher.py
@@ -1367,7 +1367,7 @@ class TensorCopy_Matcher(BaseMatcher):
             paddle.assign({}, output={})
             """
         )
-        code = API_TEMPLATE.format(kwargs["src"], self.paddleClass)
+        code = API_TEMPLATE.format(kwargs["other"], self.paddleClass)
         return code
 
 
