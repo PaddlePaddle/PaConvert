bedcc3d3a458b33294c6b9e8ebc1c1b58e03e54a
RedContritio <RedContritio@qq.com>
2023-12-06 22:39:38 +0800

    fix addr

--- tests/test_addr.py
+++ tests/test_addr.py
@@ -64,7 +64,7 @@ def test_case_4():
         a = torch.tensor([4., 5., 6.])
         b = torch.tensor([1., 2., 3.])
         input = torch.tensor([1., 2., 3.])
-        out = torch.tensor([[1., 2., 3.] * 3])
+        out = torch.zeros((3, 3))
         result = torch.addr(input=input, vec1=a, vec2=b, beta=3, alpha=3, out=out)
         """
     )
