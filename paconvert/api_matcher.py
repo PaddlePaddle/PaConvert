135ec23d0cd0c98b21b07fa6573c27a324dde00c
RedContritio <RedContritio@qq.com>
2024-01-17 15:08:25 +0800

    fix torch.XXTensor as type

--- paconvert/api_matcher.py
+++ paconvert/api_matcher.py
@@ -906,7 +906,7 @@ class TensorMatcher(BaseMatcher):
     def get_paddle_nodes(self, args, kwargs):
         kwargs = self.parse_kwargs(kwargs)
         if kwargs is None:
-            return None
+            return "paddle.Tensor"
 
         if "size" in kwargs:
             shape = kwargs.pop("size")
