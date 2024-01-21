4bc74150eff31a5470bdb053adeb4e8b54d9607f
longranger2 <836253168@qq.com>
2024-01-21 07:13:15 +0000

    add torch.optim.Optimizer.zero_grad test

---
+++ tests/test_optim_Optimizer_zero_grad.py
@@ -0,0 +1,42 @@
+# Copyright (c) 2023 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import textwrap
+
+from apibase import APIBase
+
+obj = APIBase("torch.optim.Optimizer.zero_grad")
+
+
+def test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        import torch.nn as nn
+
+        x = torch.randn(1, 2, 10)
+        model = nn.Linear(10, 20)
+        optimizer = torch.optim.Optimizer(params=model.parameters(), defaults={"learning_rate": 1.0})
+        out = model(x)
+        out.backward()
+        optimizer.step()
+        result = optimizer.zero_grad()
+        """
+    )
+    obj.run(
+        pytorch_code,
+        ["result"],
+        unsupport=True,
+        reason="paddle does not support this function temporarily",
+    )
