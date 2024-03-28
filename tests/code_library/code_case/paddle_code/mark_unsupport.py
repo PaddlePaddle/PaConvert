65b507f41e8d8c5c60cb57e4ca43706506782580
zhwesky2010 <1183042833@qq.com>
2024-03-28 16:34:12 +0800

    fix bracket in string badcase (#380)

--- tests/code_library/code_case/paddle_code/mark_unsupport.py
+++ tests/code_library/code_case/paddle_code/mark_unsupport.py
@@ -45,3 +45,36 @@ import paddlenlp
     'bert-base-chinese'), paddle.rand(shape=[2, 3, 4]), paddlenlp.
     transformers.BertTokenizer.from_pretrained('bert-base-chinese'))
 paddlenlp.transformers.BertTokenizer.from_pretrained('bert-base-chinese')
+print(
+    '#####################################case1##################################'
+    )
+source = """
+at::Tensor sin_add(at::Tensor x, at::Tensor y) {
+    return x.sin() + y.sin();
+}
+"""
+>>>>>>torch.utils.cpp_extension.load_inline(name='inline_extension', cpp_sources=
+    [source], functions=['sin_add'])
+result = True
+print('#########################case2#########################')
+print('(Torch')
+print('#########################case3#########################')
+""" This is an non-standard example
+) """
+""" (
+    This is an non-standard example
+) """
+""" (
+    This is (an non-standard example
+) """
+print('#########################case4#########################')
+print('#########################case5#########################')
+""" This is an non-standard example) """
+source = """
+at::Tensor sin_add(at::Tensor x, at::Tensor y) {
+    return x.sin() + y.sin();
+}
+"""
+>>>>>>torch.utils.cpp_extension.load_inline(name='inline_extension', cpp_sources=
+    [source], functions=['sin_add'])
+result = True
