f263832c6464ea5b956e595271a011268dcc9066
1147022743 <1147022743@qq.com>
2024-05-31 13:19:24 +0800

    Update test_dsplit.py

--- tests/test_dsplit.py
+++ tests/test_dsplit.py
@@ -30,9 +30,7 @@ def test_case_1():
     )
     obj.run(
         pytorch_code,
-        ["result"],
-        unsupport=True,
-        reason="paddle does not support this function temporarily",
+        ["result"]
     )
 
 
@@ -48,9 +46,7 @@ def test_case_2():
     )
     obj.run(
         pytorch_code,
-        ["result"],
-        unsupport=True,
-        reason="paddle does not support this function temporarily",
+        ["result"]
     )
 
 
@@ -66,7 +62,5 @@ def test_case_3():
     )
     obj.run(
         pytorch_code,
-        ["result"],
-        unsupport=True,
-        reason="paddle does not support this function temporarily",
+        ["result"]
     )
