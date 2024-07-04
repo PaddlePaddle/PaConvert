2b48cebe05e5c0ae03c95bfecbe35ce875ab0400
1147022743 <1147022743@qq.com>
2024-07-04 21:38:39 +0800

    Update test_hsplit.py
    提交八个算子——修改补充

--- tests/test_hsplit.py
+++ tests/test_hsplit.py
@@ -28,12 +28,7 @@ def test_case_1():
         result = torch.hsplit(t, 2)
         """
     )
-    obj.run(
-        pytorch_code,
-        ["result"],
-        unsupport=True,
-        reason="paddle does not support this function temporarily",
-    )
+    obj.run(pytorch_code, ["result"])
 
 
 def test_case_2():
@@ -44,12 +39,7 @@ def test_case_2():
         result = torch.hsplit(t, [3, 6])
         """
     )
-    obj.run(
-        pytorch_code,
-        ["result"],
-        unsupport=True,
-        reason="paddle does not support this function temporarily",
-    )
+    obj.run(pytorch_code, ["result"])
 
 
 def test_case_3():
@@ -60,9 +50,15 @@ def test_case_3():
         result = torch.hsplit(t, indices=[1, 2])
         """
     )
-    obj.run(
-        pytorch_code,
-        ["result"],
-        unsupport=True,
-        reason="paddle does not support this function temporarily",
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        t = torch.arange(16.0).reshape(4, 4)
+        result = torch.hsplit(t,sections=2)
+        """
     )
+    obj.run(pytorch_code, ["result"])
