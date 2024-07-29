9448f4b37c77f80875b98adffe4b2c24846e955c
1147022743 <1147022743@qq.com>
2024-07-29 12:15:45 +0800

    提交八个算子——修改补充2 (#435)
    * Update api_mapping.json
    
    提交八个算子——修改补充
    
    * Update api_matcher.py
    
    提交八个算子——修改补充
    
    * Update test_Tensor_dsplit.py
    
    提交八个算子——修改补充
    
    * Update test_dsplit.py
    
    提交八个算子——修改补充
    
    * Update test_Tensor_hsplit.py
    
    提交八个算子——修改补充
    
    * Update test_hsplit.py
    
    提交八个算子——修改补充
    
    * Update test_Tensor_select_scatter.py
    
    提交八个算子——修改补充
    
    * Update test_Tensor_slice_scatter.py
    
    提交八个算子——修改补充
    
    * Add files via upload
    
    * Add files via upload
    
    * 提交八个算子——修改补充2
    
    * 提交八个算子——修改补充2(这里不能简化，会导致报错）

---
+++ tests/test_Tensor_tensor_split.py
@@ -0,0 +1,86 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import textwrap
+
+from apibase import APIBase
+
+obj = APIBase("torch.Tensor.tensor_split")
+
+
+def test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(8)
+        result = a.tensor_split(4)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(7)
+        result = a.tensor_split(sections = 3)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(7)
+        result = a.tensor_split((1, 6))
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(14).reshape(2, 7)
+        result = a.tensor_split(3, dim = 1)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(14).reshape(2, 7)
+        result = a.tensor_split(indices = (1, 6), dim = 1)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_6():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(14).reshape(2, 7)
+        result = a.tensor_split(3, 1)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
---
+++ tests/test_tensor_split2.py
@@ -0,0 +1,119 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import textwrap
+
+from apibase import APIBase
+
+obj = APIBase("torch.tensor_split")
+
+
+def test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(8)
+        result = torch.tensor_split(a, 3)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(7)
+        result = torch.tensor_split(a, sections = 3)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(7)
+        result = torch.tensor_split(a, (1, 6))
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(7)
+        result = torch.tensor_split(a, indices = (1, 6))
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(7)
+        result = torch.tensor_split(a, [1, 6])
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_6():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(7)
+        result = torch.tensor_split(a, indices = [1, 6])
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_7():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(14).reshape(2, 7)
+        result = torch.tensor_split(a, 3, dim = 1)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_8():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(14).reshape(2, 7)
+        result = torch.tensor_split(a, (1, 6), dim = 1)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_9():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.arange(14).reshape(2, 7)
+        result = torch.tensor_split(a, 3, 1)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
