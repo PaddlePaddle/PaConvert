28ed5355f8a2d9504a4a9f7ad9238050f8eca1fc
enkilee <jeffrey0122@163.com>
2024-09-19 11:49:52 +0800

    fix

---
+++ tests/test_signal_windows_exponential.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2024 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import textwrap
+
+from apibase import APIBase
+
+obj = APIBase("torch.signal.windows.exponential")
+
+
+def test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.signal.windows.exponential(10)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.signal.windows.exponential(10, dtype=torch.float64)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.signal.windows.exponential(10, tau=.5, dtype=torch.float64)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.signal.windows.exponential(10, dtype=torch.float64, requires_grad=True)
+        """
+    )
+    obj.run(pytorch_code, ["out"])
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.signal.windows.exponential(10, dtype=torch.float64, layout=torch.strided, requires_grad=True)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+def test_case_6():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.signal.windows.exponential(10, dtype=torch.float64, layout=torch.strided, device=torch.device('cpu'), requires_grad=True)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+def test_case_7():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        result = torch.signal.windows.exponential(10, sym=False, dtype=torch.float64)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
