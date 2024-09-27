3c5e0e4a4e7c3f3e0900b79c236e831f032def1e
enkilee <jeffrey0122@163.com>
2024-09-27 12:28:10 +0800

    fix

--- paconvert/api_matcher.py
+++ paconvert/api_matcher.py
@@ -541,12 +541,8 @@ class SignalWindowsWatcher(BaseMatcher):
             else:
                 new_kwargs["window"] = ("general_hamming", 0.54)
         if "general_cosine" in self.torch_api:
-            if "a" in kwargs:
-                print("------kwargs-------", kwargs.values())
-                a_value = list(kwargs.values())[0]
-                new_kwargs["window"] = ("general_cosine", a_value)
-            else:
-                new_kwargs["window"] = ("general_cosine", [0.46, 0.23, 0.31])
+            a_value = [v for v in kwargs.values()][0]
+            new_kwargs["window"] = ("general_cosine", a_value)
         new_kwargs.update(kwargs)
         return GenericMatcher.generate_code(self, new_kwargs)
 
