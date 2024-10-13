1906d02c9b7c05857aaddb443e309027b70ee310
xxa <1829994704@qq.com>
2024-10-14 00:33:01 +0800

    fix docs api

--- tests/test_cuda_nvtx_range_push.py
+++ tests/test_cuda_nvtx_range_push.py
@@ -18,7 +18,26 @@ import paddle
 import pytest
 from apibase import APIBase
 
-obj = APIBase("torch.cuda.nvtx.range_push")
+
+class cudaNvxtRangePushAPI(APIBase):
+    def compare(
+        self,
+        name,
+        pytorch_result,
+        paddle_result,
+        check_value=True,
+        check_dtype=True,
+        check_stop_gradient=True,
+        rtol=1.0e-6,
+        atol=0.0,
+    ):
+        if paddle_result is not None and paddle_result != pytorch_result:
+            assert (
+                False
+            ), f"Results do not match: PaddlePaddle result is {paddle_result}, PyTorch result is {pytorch_result}"
+
+
+obj = cudaNvxtRangePushAPI("torch.cuda.nvtx.range_push")
 
 
 @pytest.mark.skipif(
@@ -29,8 +48,7 @@ def test_case_1():
     pytorch_code = textwrap.dedent(
         """
         import torch
-        torch.cuda.nvtx.range_push("msg")
-        result = 0
+        result = torch.cuda.nvtx.range_push("msg")
         """
     )
     obj.run(pytorch_code, ["result"])
@@ -44,8 +62,7 @@ def test_case_2():
     pytorch_code = textwrap.dedent(
         """
         import torch
-        torch.cuda.nvtx.range_push(msg="msg")
-        result = 0
+        result = torch.cuda.nvtx.range_push(msg="msg")
         """
     )
     obj.run(pytorch_code, ["result"])
