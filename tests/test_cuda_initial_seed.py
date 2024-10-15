78dbe35677bfee51288069911c80da5c12626e93
xxa <1829994704@qq.com>
2024-10-15 21:31:39 +0800

    fix docs api

--- tests/test_cuda_initial_seed.py
+++ tests/test_cuda_initial_seed.py
@@ -21,7 +21,6 @@ from apibase import APIBase
 obj = APIBase("torch.cuda.initial_seed")
 
 
-
 @pytest.mark.skipif(
     condition=not paddle.device.is_compiled_with_cuda(),
     reason="can only run on paddle with CUDA",
