271be5dc4b151eed0101849ff904a7bbd7013abb
decade-afk <3995409050@qq.com>
2024-10-16 12:43:25 +0800

    add remote

--- tests/test_distributed_rpc_remote.py
+++ tests/test_distributed_rpc_remote.py
@@ -16,7 +16,7 @@ import textwrap
 
 from apibase import APIBase
 
-obj = APIBase("torch.distributed.rpc.remote")
+obj = APIBase("torch.distributed.rpc.remote", is_aux_api=True)
 
 
 def test_case_1():
