3f8164784dcb37f19ebdb57b9712249460620825
zhouwei25 <zhouwei25@baidu.com>
2024-10-28 07:59:10 +0000

    echo torch version

--- scripts/code_unittest_check_gpu.sh
+++ scripts/code_unittest_check_gpu.sh
@@ -18,6 +18,9 @@ export FLAGS_set_to_1d=0
 
 export LD_LIBRARY_PATH=/root/anaconda3/lib:$LD_LIBRARY_PATH
 
+echo "Insalling cpu version torch"
+python -c "import torch; print('torch version information:' ,torch.__version__)"
+
 echo "Insalling develop gpu version paddle"
 python -m pip uninstall -y paddlepaddle
 python -m pip uninstall -y paddlepaddle-gpu
