90cb8ce0e8fc34d587ca2a9e971c73f240741f76
guozixu2001 <guozixu2001@gmail.com>
2024-11-05 11:40:20 +0800

    fix small issues

--- tests/torchvision_tests/test_ColorJitter.py
+++ tests/torchvision_tests/test_ColorJitter.py
@@ -38,7 +38,7 @@ def test_case_2():
         import torch
         from torchvision.transforms import ColorJitter
         img = torch.tensor([[[0.1, 0.2], [0.3, 0.4]], [[0.5, 0.6], [0.7, 0.8]], [[0.9, 1.0], [1.0, 1.0]]])
-        jitter = ColorJitter(0, 0.3, 0.4, 0.1)
+        jitter = ColorJitter(0, 0.3, 0.4, 0)
         result = jitter(img)
         """
     )
