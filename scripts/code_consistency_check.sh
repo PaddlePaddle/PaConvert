66b3cef04e1eb6ba7d375691344fddc0e4e97cfd
zhwesky2010 <1183042833@qq.com>
2024-11-19 17:17:36 +0800

    fix CI and code style (#512)

--- scripts/code_consistency_check.sh
+++ scripts/code_consistency_check.sh
@@ -14,22 +14,10 @@
 
 set +x
 
-export FLAGS_set_to_1d=0
 DOWNLOAD_DATASET_IF="OFF"
 
 cd /workspace/$1/PaConvert/
 TORCH_PROJECT_PATH=$2
-
-echo "Insalling latest release cpu version torch"
-python -m pip install torch==2.1.0 --index-url https://download.pytorch.org/whl/cpu
-python -c "import torch; print('torch version information:' ,torch.__version__)"
-
-echo "Insalling develop cpu version paddle"
-python -m pip uninstall -y paddlepaddle
-python -m pip uninstall -y paddlepaddle-gpu
-rm -rf /root/anaconda3/lib/python*/site-packages/paddlepaddle-0.0.0.dist-info/
-python -m pip install --pre paddlepaddle -i https://www.paddlepaddle.org.cn/packages/nightly/cpu/
-python -c "import paddle; print('paddle version information:' , paddle.__version__); commit = paddle.__git_commit__;print('paddle commit information:' , commit)"
 
 echo "Installing dependencies"
 python -m pip install pandas openpyxl
--- scripts/code_coverage_check.sh
+++ scripts/code_coverage_check.sh
@@ -14,20 +14,7 @@
 
 set +x
 
-export FLAGS_set_to_1d=0
 cd /workspace/$1/PaConvert/
-
-echo "Insalling latest release cpu version torch"
-python tests/distributed/load_lib.py
-python -m pip install torch --index-url https://download.pytorch.org/whl/cpu
-python -c "import torch; print('torch version information:' ,torch.__version__)"
-
-echo "Insalling develop cpu version paddle"
-python -m pip uninstall -y paddlepaddle
-python -m pip uninstall -y paddlepaddle-gpu
-rm -rf /root/anaconda3/lib/python*/site-packages/paddlepaddle-0.0.0.dist-info/
-python -m pip install --pre paddlepaddle -i https://www.paddlepaddle.org.cn/packages/nightly/cpu/
-python -c "import paddle; print('paddle version information:' , paddle.__version__); commit = paddle.__git_commit__;print('paddle commit information:' , commit)"
 
 # use Coverage diff-cover
 echo "Insalling coverage and diff-cover for incremental code inspection"
