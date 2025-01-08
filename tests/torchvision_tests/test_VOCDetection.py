18f6c24e1aaa72640b2aa5ba72016e000276f198
guozixu2001 <guozixu2001@gmail.com>
2025-01-08 12:55:19 +0800

    add tests

--- tests/torchvision_tests/test_VOCDetection.py
+++ tests/torchvision_tests/test_VOCDetection.py
@@ -13,8 +13,265 @@
 # limitations under the License.
 #
 
+import textwrap
+
 from apibase import APIBase
 
 obj = APIBase("torchvision.datasets.VOCDetection")
 
-# TODO(guozixu): Complete test cases after paddle_aux refactoring is done
+
+def test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torchvision
+        root_path = './data'
+        train_dataset = torchvision.datasets.VOCDetection(root=root_path, image_set='trainval')
+        """
+    )
+    paddle_code = textwrap.dedent(
+        """
+        import os
+
+        import paddle
+
+        ############################## 相关utils函数，如下 ##############################
+
+
+        def VOCDetection(*args, **kwargs):
+            root = kwargs.pop("root")
+            year = kwargs.pop("year", "2012")
+            if year != "2012":
+                raise ValueError("PaddlePaddle only supports VOC2012 dataset")
+            image_set = kwargs.pop("image_set", "train")
+            download = kwargs.pop("download", True)
+            transform = kwargs.pop("transform", None)
+
+            if image_set == "trainval":
+                mode = "train"
+            elif image_set == "train":
+                mode = "test"
+            elif image_set == "val":
+                mode = "valid"
+            else:
+                raise ValueError("Only supports image_set in ['trainval', 'train', 'val']")
+
+            data_file = os.path.join(root, "VOCtrainval_11-May-2012.tar")
+            return paddle.vision.datasets.VOC2012(data_file=data_file, mode=mode, transform=transform, download=download, backend=None)
+
+        ############################## 相关utils函数，如上 ##############################
+
+
+        root_path = "./data"
+        train_dataset = VOCDetection(root=root_path, image_set="trainval")
+        """
+    )
+    obj.run(
+        pytorch_code,
+        expect_paddle_code=paddle_code,
+    )
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torchvision
+        root_path = './data'
+        train_dataset = torchvision.datasets.VOCDetection(root=root_path, image_set='train')
+        """
+    )
+    paddle_code = textwrap.dedent(
+        """
+        import os
+
+        import paddle
+
+        ############################## 相关utils函数，如下 ##############################
+
+
+        def VOCDetection(*args, **kwargs):
+            root = kwargs.pop("root")
+            year = kwargs.pop("year", "2012")
+            if year != "2012":
+                raise ValueError("PaddlePaddle only supports VOC2012 dataset")
+            image_set = kwargs.pop("image_set", "train")
+            download = kwargs.pop("download", True)
+            transform = kwargs.pop("transform", None)
+
+            if image_set == "trainval":
+                mode = "train"
+            elif image_set == "train":
+                mode = "test"
+            elif image_set == "val":
+                mode = "valid"
+            else:
+                raise ValueError("Only supports image_set in ['trainval', 'train', 'val']")
+
+            data_file = os.path.join(root, "VOCtrainval_11-May-2012.tar")
+            return paddle.vision.datasets.VOC2012(data_file=data_file, mode=mode, transform=transform, download=download, backend=None)
+
+        ############################## 相关utils函数，如上 ##############################
+
+
+        root_path = "./data"
+        train_dataset = VOCDetection(root=root_path, image_set="train")
+        """
+    )
+    obj.run(
+        pytorch_code,
+        expect_paddle_code=paddle_code,
+    )
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torchvision
+        root_path = './data'
+        train_dataset = torchvision.datasets.VOCDetection(root_path)
+        """
+    )
+    paddle_code = textwrap.dedent(
+        """
+        import os
+
+        import paddle
+
+        ############################## 相关utils函数，如下 ##############################
+
+
+        def VOCDetection(*args, **kwargs):
+            root = kwargs.pop("root")
+            year = kwargs.pop("year", "2012")
+            if year != "2012":
+                raise ValueError("PaddlePaddle only supports VOC2012 dataset")
+            image_set = kwargs.pop("image_set", "train")
+            download = kwargs.pop("download", True)
+            transform = kwargs.pop("transform", None)
+
+            if image_set == "trainval":
+                mode = "train"
+            elif image_set == "train":
+                mode = "test"
+            elif image_set == "val":
+                mode = "valid"
+            else:
+                raise ValueError("Only supports image_set in ['trainval', 'train', 'val']")
+
+            data_file = os.path.join(root, "VOCtrainval_11-May-2012.tar")
+            return paddle.vision.datasets.VOC2012(data_file=data_file, mode=mode, transform=transform, download=download, backend=None)
+
+        ############################## 相关utils函数，如上 ##############################
+
+
+        root_path = "./data"
+        train_dataset = VOCDetection(root=root_path)
+        """
+    )
+    obj.run(
+        pytorch_code,
+        expect_paddle_code=paddle_code,
+    )
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torchvision
+        root_path = './data'
+        train_dataset = torchvision.datasets.VOCDetection(image_set='trainval', download=False, root=root_path)
+        """
+    )
+    paddle_code = textwrap.dedent(
+        """
+        import os
+
+        import paddle
+
+        ############################## 相关utils函数，如下 ##############################
+
+
+        def VOCDetection(*args, **kwargs):
+            root = kwargs.pop("root")
+            year = kwargs.pop("year", "2012")
+            if year != "2012":
+                raise ValueError("PaddlePaddle only supports VOC2012 dataset")
+            image_set = kwargs.pop("image_set", "train")
+            download = kwargs.pop("download", True)
+            transform = kwargs.pop("transform", None)
+
+            if image_set == "trainval":
+                mode = "train"
+            elif image_set == "train":
+                mode = "test"
+            elif image_set == "val":
+                mode = "valid"
+            else:
+                raise ValueError("Only supports image_set in ['trainval', 'train', 'val']")
+
+            data_file = os.path.join(root, "VOCtrainval_11-May-2012.tar")
+            return paddle.vision.datasets.VOC2012(data_file=data_file, mode=mode, transform=transform, download=download, backend=None)
+
+        ############################## 相关utils函数，如上 ##############################
+
+
+        root_path = "./data"
+        train_dataset = VOCDetection(image_set="trainval", download=False, root=root_path)
+        """
+    )
+    obj.run(
+        pytorch_code,
+        expect_paddle_code=paddle_code,
+    )
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torchvision
+        root_path = './data'
+        train_dataset = torchvision.datasets.VOCDetection(root=root_path, image_set='trainval', transform=None, download=False)
+        """
+    )
+    paddle_code = textwrap.dedent(
+        """
+        import os
+
+        import paddle
+
+        ############################## 相关utils函数，如下 ##############################
+
+
+        def VOCDetection(*args, **kwargs):
+            root = kwargs.pop("root")
+            year = kwargs.pop("year", "2012")
+            if year != "2012":
+                raise ValueError("PaddlePaddle only supports VOC2012 dataset")
+            image_set = kwargs.pop("image_set", "train")
+            download = kwargs.pop("download", True)
+            transform = kwargs.pop("transform", None)
+
+            if image_set == "trainval":
+                mode = "train"
+            elif image_set == "train":
+                mode = "test"
+            elif image_set == "val":
+                mode = "valid"
+            else:
+                raise ValueError("Only supports image_set in ['trainval', 'train', 'val']")
+
+            data_file = os.path.join(root, "VOCtrainval_11-May-2012.tar")
+            return paddle.vision.datasets.VOC2012(data_file=data_file, mode=mode, transform=transform, download=download, backend=None)
+
+        ############################## 相关utils函数，如上 ##############################
+
+
+        root_path = "./data"
+        train_dataset = VOCDetection(
+            root=root_path, image_set="trainval", transform=None, download=False
+        )
+        """
+    )
+    obj.run(
+        pytorch_code,
+        expect_paddle_code=paddle_code,
+    )
