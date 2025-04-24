3c4951ad97c5927efb0203c4040e2d550e3ab3d1
zhouwei25 <zhouwei25@baidu.com>
2025-04-24 12:13:43 +0800

    更新GPU CI的torch版本

--- scripts/unittest_check_gpu.sh
+++ scripts/unittest_check_gpu.sh
@@ -14,13 +14,14 @@
 
 set +x
 
-echo "Insalling cpu version torch"
+echo "Insalling latest release gpu version torch"
+# install in Dockerfile
+# python -m pip install -U torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 python -c "import torch; print('torch version information:' ,torch.__version__)"
 
 echo "Insalling develop gpu version paddle"
 python -m pip uninstall -y paddlepaddle
 python -m pip uninstall -y paddlepaddle-gpu
-rm -rf /root/anaconda3/lib/python*/site-packages/paddlepaddle-0.0.0.dist-info/
 python -m pip install --pre paddlepaddle-gpu -i https://www.paddlepaddle.org.cn/packages/nightly/cu118/
 python -c "import paddle; print('paddle version information:' , paddle.__version__); commit = paddle.__git_commit__;print('paddle commit information:' , commit)"
 
