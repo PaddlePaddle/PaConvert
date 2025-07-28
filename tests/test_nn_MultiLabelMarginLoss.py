25db9f6c5e9419d6fbb38a6dc0e26343a93ed894
Qin-sx <67671068+Qin-sx@users.noreply.github.com>
2025-07-28 15:38:35 +0800

    新增MultiLabelMarginLoss (#599)
    * added MultiLabelMarginLoss
    
    	modified:   paconvert/api_mapping.json
    	modified:   tests/test_nn_MultiLabelMarginLoss.py
    	modified:   tests/test_nn_functional_multilabel_margin_loss.py
    
    * added more tests
    
    	modified:   tests/test_nn_MultiLabelMarginLoss.py
    	modified:   tests/test_nn_functional_multilabel_margin_loss.py
    
    * comment case 6
    
    	modified:   tests/test_nn_MultiLabelMarginLoss.py
    	modified:   tests/test_nn_functional_multilabel_margin_loss.py

--- tests/test_nn_MultiLabelMarginLoss.py
+++ tests/test_nn_MultiLabelMarginLoss.py
@@ -12,21 +12,112 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import textwrap
+import textwrap
 
-# from apibase import APIBase
+from apibase import APIBase
 
-# obj = APIBase("torch.nn.MultiLabelMarginLoss")
+obj = APIBase("torch.nn.MultiLabelMarginLoss")
 
 
-# def _test_case_1():
-#     pytorch_code = textwrap.dedent(
-#         """
-#         import torch
-#         loss = torch.nn.MultiLabelMarginLoss(size_average=True, reduce=False, reduction="mean")
-#         input = torch.tensor([[1, -2, 3], [0, -1, 2], [1, 0, 1]]).to(dtype=torch.float32)
-#         label = torch.LongTensor([[-1, 1, -1], [1, 1, 1], [1, -1, 1]])
-#         result = loss(input, label)
-#         """
-#     )
-#     obj.run(pytorch_code, ["result"])
+def test_case_1():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.MultiLabelMarginLoss(size_average=True, reduce=False, reduction="mean")
+        input = torch.tensor([[1, -2, 3], [0, -1, 2], [1, 0, 1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[-1, 1, -1], [1, 1, 1], [1, -1, 1]])
+        result = loss(input, label)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.MultiLabelMarginLoss(size_average=True, reduce=False, reduction="mean")
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8], [0.2, 0.5, 0.3, 0.1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, -1], [0, 2, -1, -1]])
+        result = loss(input, label)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.MultiLabelMarginLoss(size_average=True, reduce=False, reduction="sum")
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8], [0.2, 0.5, 0.3, 0.1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, -1], [0, 2, -1, -1]])
+        result = loss(input, label)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.MultiLabelMarginLoss(size_average=True, reduce=False, reduction="none")
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8], [0.2, 0.5, 0.3, 0.1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, -1], [0, 2, -1, -1]])
+        result = loss(input, label)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.MultiLabelMarginLoss(reduction="mean")
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, -1]])
+        result = loss(input, label)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def _test_case_6():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.MultiLabelMarginLoss(reduction="none")
+        input = torch.tensor([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[-1, -1, -1], [-1, -1, -1]])
+        result = loss(input, label)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_7():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.MultiLabelMarginLoss(reduction="sum")
+        input = torch.tensor([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[0, 1, 2], [0, 1, 2]])
+        result = loss(input, label)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_8():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.MultiLabelMarginLoss(size_average=None, reduce=True)
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8], [0.2, 0.5, 0.3, 0.1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, -1], [0, 2, -1, -1]])
+        result = loss(input, label)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
--- tests/test_nn_functional_multilabel_margin_loss.py
+++ tests/test_nn_functional_multilabel_margin_loss.py
@@ -29,6 +29,91 @@ def test_case_1():
         result = loss(input, label)
         """
     )
-    obj.run(
-        pytorch_code, ["result"], unsupport=True, reason="The API is not supported."
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.functional.multilabel_margin_loss
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8], [0.2, 0.5, 0.3, 0.1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, 1], [0, 2, -1, -1]])
+        result = loss(input, label)
+        """
     )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.functional.multilabel_margin_loss
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8], [0.2, 0.5, 0.3, 0.1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, 1], [0, 2, -1, -1]])
+        result = loss(input, label, reduction='sum')
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_4():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        loss = torch.nn.functional.multilabel_margin_loss
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8], [0.2, 0.5, 0.3, 0.1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, 1], [0, 2, -1, -1]])
+        result = loss(input, label, reduction='none')
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_5():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        input = torch.tensor([[1, -2, 3], [0, -1, 2], [1, 0, 1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[-1, 1, -1], [1, 1, 1], [1, -1, 1]])
+        result = torch.nn.functional.multilabel_margin_loss(input, label, size_average=True, reduce=False, reduction="mean")
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+# def test_case_6():
+#     pytorch_code = textwrap.dedent(
+#         """
+#         import torch
+#         input = torch.tensor([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]).to(dtype=torch.float32)
+#         label = torch.LongTensor([[-1, -1, -1], [-1, -1, -1]])
+#         result = torch.nn.functional.multilabel_margin_loss(input, label, reduction="none")
+#         """
+#     )
+#     obj.run(pytorch_code, ["result"])
+
+
+def test_case_7():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        input = torch.tensor([[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[0, 1, 2], [0, 1, 2]])
+        result = torch.nn.functional.multilabel_margin_loss(input, label, reduction="sum")
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_8():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        input = torch.tensor([[0.1, 0.2, 0.4, 0.8], [0.2, 0.5, 0.3, 0.1]]).to(dtype=torch.float32)
+        label = torch.LongTensor([[3, 0, -1, -1], [0, 2, -1, -1]])
+        result = torch.nn.functional.multilabel_margin_loss(input, label, size_average=None, reduce=True)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
