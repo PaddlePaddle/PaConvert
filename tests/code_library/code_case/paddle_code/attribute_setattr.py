7bfa7c6abcb9cca7ea53338362c4d1309b948b9a
zhiminzhang0830 <452516515@qq.com>
2025-08-28 04:15:09 +0000

    change Conv2D to Conv2d

--- tests/code_library/code_case/paddle_code/attribute_setattr.py
+++ tests/code_library/code_case/paddle_code/attribute_setattr.py
@@ -9,8 +9,8 @@ paddle.nn.Layer.add_sublayer = add_module
 paddle.nn.Layer.add_sublayer = add_module
 paddle.nn.Layer.add_sublayer = add_module
 network = paddle.nn.Sequential(
-    paddle.nn.Conv2D(in_channels=1, out_channels=20, kernel_size=5),
-    paddle.nn.Conv2D(in_channels=20, out_channels=64, kernel_size=5),
+    paddle.nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5),
+    paddle.nn.Conv2d(in_channels=20, out_channels=64, kernel_size=5),
 )
 network.add_sublayer(name="ReLU", sublayer=paddle.nn.ReLU())
 print(network)
