55b766bf3cd51b303232dda7628bfe729fe776d4
Zero Rains <linjunlu@zerorains.top>
2025-08-28 11:14:20 +0800

    [API Compatibility] Add paddle.Tensor.norm, paddle.as_tensor and paddle.[dtype]Tensor (#662)
    * paddle.nrom
    
    * fix codecosistency
    
    * fix bug

--- tests/code_library/code_case/paddle_code/type_hinting.py
+++ tests/code_library/code_case/paddle_code/type_hinting.py
@@ -3,13 +3,13 @@ from typing import List, Optional, Tuple, Union
 import paddle
 
 print("#########################case1#########################")
-Union[Tuple, paddle.Tensor]
+Union[Tuple, paddle.BoolTensor]
 print("#########################case2#########################")
-paddle.tensor([True, False], dtype="bool")
+paddle.BoolTensor([True, False])
 print("#########################case3#########################")
 
 
-def fun(b: paddle.Tensor):
+def fun(b: paddle.BoolTensor):
     pass
 
 
@@ -19,13 +19,13 @@ print("#########################case5#########################")
 
 
 def forward(
-    hidden_states: Optional[Tuple[paddle.Tensor]],
+    hidden_states: Optional[Tuple[paddle.FloatTensor]],
     rotary_pos_emb_list: Optional[List[List[paddle.Tensor]]] = None,
     layer_past: Optional[Tuple[paddle.Tensor]] = None,
-    attention_mask: Optional[paddle.Tensor] = None,
-    head_mask: Optional[paddle.Tensor] = None,
+    attention_mask: Optional[paddle.FloatTensor] = None,
+    head_mask: Optional[paddle.FloatTensor] = None,
     encoder_hidden_states: Optional[paddle.Tensor] = None,
-    encoder_attention_mask: Optional[paddle.Tensor] = None,
+    encoder_attention_mask: Optional[paddle.FloatTensor] = None,
     output_attentions: Optional[bool] = False,
     use_cache: Optional[bool] = False,
 ):
