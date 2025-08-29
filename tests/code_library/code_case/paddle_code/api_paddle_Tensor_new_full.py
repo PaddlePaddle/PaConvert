8af7302a5d1b9267f172fe9c78c6fae9194f6426
Difers <707065510@qq.com>
2025-08-29 14:57:16 +0800

    fix consistency

--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_full.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_full.py
@@ -1,13 +1,13 @@
 import paddle
 
 print("#########################case1#########################")
-a = paddle.tensor([1, 2, 3], dtype="int64")
-out_0 = paddle.full(shape=[3, 4], fill_value=2.43, dtype="float64")
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
+out_0 = paddle.full(shape=[3, 4], fill_value=2.43, dtype=paddle.float64)
 out_0.stop_gradient = not True
 b = out_0.pin_memory()
 print("#########################case2#########################")
 flag = False
-a = paddle.tensor([1, 2, 3], dtype="int64")
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
 out_1 = paddle.full(shape=(2, 3), fill_value=4, dtype=a.dtype)
 out_1.stop_gradient = not flag
 b = out_1
