92ae0fef802e742afc0e3c4756f1432939963d9b
HydrogenSulfate <490868991@qq.com>
2025-08-29 17:32:01 +0800

    fix

--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_empty.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_empty.py
@@ -1,9 +1,9 @@
 import paddle
 
 print("#########################case1#########################")
-a = paddle.to_tensor(data=[1, 2, 3], dtype="int64")
+a = paddle.tensor([1, 2, 3], dtype="int64")
 b = a.new_empty([3, 4], dtype="float64", requires_grad=True, pin_memory=True)
 print("#########################case2#########################")
 flag = False
-a = paddle.to_tensor(data=[1, 2, 3], dtype="int64")
+a = paddle.tensor([1, 2, 3], dtype="int64")
 b = a.new_empty((2, 3), requires_grad=flag)
--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_ones.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_ones.py
@@ -2,13 +2,9 @@ import paddle
 
 print("#########################case1#########################")
 a = paddle.tensor([1, 2, 3], dtype="int64")
-out_0 = paddle.ones(shape=[3, 4], dtype="float64")
-out_0.stop_gradient = not True
-b = out_0.pin_memory()
+b = a.new_ones([3, 4], dtype="float64", requires_grad=True, pin_memory=True)
 print("#########################case2#########################")
 a = paddle.tensor([1, 2, 3], dtype="int64")
-out_1 = paddle.ones(shape=[3, 4], dtype=a.dtype)
-out_1.stop_gradient = not True
-b = out_1
+b = a.new_ones(3, 4, requires_grad=True)
 print("#########################case3#########################")
 b = a.new_ones([3, 4])
