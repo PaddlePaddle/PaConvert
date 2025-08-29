9cf556e288fc705717b8de485388cbccd3036322
zhengshengning <ningzhengsheng@baidu.com>
2025-08-29 12:09:58 +0800

    fix

--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_expand.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_expand.py
@@ -1,8 +1,8 @@
 import paddle
 
 print("#########################case1#########################")
-x = paddle.to_tensor(data=[[1], [2], [3]])
-y = x.expand(shape=[3, 4])
+x = paddle.tensor([[1], [2], [3]])
+y = x.expand(3, 4)
 print("#########################case2#########################")
-x = paddle.to_tensor(data=[[1], [2], [3]])
-y = x.expand(shape=(3, 4))
+x = paddle.tensor([[1], [2], [3]])
+y = x.expand((3, 4))
