6d01715761fe8a3ed24ee36756c1b04a3c875549
DanielSun11 <1395924413@qq.com>
2025-08-29 10:56:24 +0800

    convert

--- tests/code_library/code_case/paddle_code/insert_pass_in_ast_if.py
+++ tests/code_library/code_case/paddle_code/insert_pass_in_ast_if.py
@@ -19,7 +19,7 @@ if TYPE_CHECKING:
     a = paddle.randn(shape=[10, 20])
     b = paddle.randn(shape=[10, 20])
     c = paddle.matmul(x=a, y=b)
-    d = paddle.add(a, b)
+    d = paddle.add(x=a, y=paddle.to_tensor(b))
     numpy.array([1, 2, 3])
 else:
     warnings.warn("The above code is meaningless, in order to pass the code style CI")
