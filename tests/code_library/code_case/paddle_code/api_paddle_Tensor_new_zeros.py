4e1588d952c5f2dee7e1c804da550a51f1007623
HydrogenSulfate <490868991@qq.com>
2025-08-29 23:49:49 +0800

    fix

--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_zeros.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_zeros.py
@@ -1,10 +1,10 @@
 import paddle
 
 print("#########################case1#########################")
-a = paddle.tensor([1, 2, 3], dtype="int64")
-b = a.new_zeros([3, 4], dtype="float64", requires_grad=True, pin_memory=True)
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
+b = a.new_zeros([3, 4], dtype=paddle.float64, requires_grad=True, pin_memory=True)
 print("#########################case2#########################")
-a = paddle.tensor([1, 2, 3], dtype="int64")
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
 b = a.new_zeros(3, 4, requires_grad=True)
 print("#########################case3#########################")
 b = a.new_zeros([3, 4])
