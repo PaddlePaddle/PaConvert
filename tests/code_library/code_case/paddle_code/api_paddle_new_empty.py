facaecf4152a82d2c7ebe2af21c7ca3b24f07074
HydrogenSulfate <490868991@qq.com>
2025-08-30 17:18:18 +0800

    fix api_paddle_new_empty.py

--- tests/code_library/code_case/paddle_code/api_paddle_new_empty.py
+++ tests/code_library/code_case/paddle_code/api_paddle_new_empty.py
@@ -1,5 +1,5 @@
 import paddle
 
 print("#########################case1#########################")
-a = paddle.tensor([1, 2, 3], dtype="int64")
-b = a.new_empty((3, 4), dtype="float64", requires_grad=True, pin_memory=True)
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
+b = a.new_empty((3, 4), dtype=paddle.float64, requires_grad=True, pin_memory=True)
