75769cd8bb99c121535fe2e6c1428bc249d51d7a
HydrogenSulfate <490868991@qq.com>
2025-08-30 10:05:00 +0800

    update

--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_empty.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_empty.py
@@ -1,9 +1,9 @@
 import paddle
 
 print("#########################case1#########################")
-a = paddle.tensor([1, 2, 3], dtype="int64")
-b = a.new_empty([3, 4], dtype="float64", requires_grad=True, pin_memory=True)
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
+b = a.new_empty([3, 4], dtype=paddle.float64, requires_grad=True, pin_memory=True)
 print("#########################case2#########################")
 flag = False
-a = paddle.tensor([1, 2, 3], dtype="int64")
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
 b = a.new_empty((2, 3), requires_grad=flag)
--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_full.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_new_full.py
@@ -1,9 +1,9 @@
 import paddle
 
 print("#########################case1#########################")
-a = paddle.tensor([1, 2, 3], dtype="int64")
-b = a.new_full([3, 4], 2.43, dtype="float64", requires_grad=True, pin_memory=True)
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
+b = a.new_full([3, 4], 2.43, dtype=paddle.float64, requires_grad=True, pin_memory=True)
 print("#########################case2#########################")
 flag = False
-a = paddle.tensor([1, 2, 3], dtype="int64")
+a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
 b = a.new_full((2, 3), 4, requires_grad=flag)
