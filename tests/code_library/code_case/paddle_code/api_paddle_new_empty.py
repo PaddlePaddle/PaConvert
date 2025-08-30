2086729c96e3106b4c564c1df51fda913c57a9a2
HydrogenSulfate <490868991@qq.com>
2025-08-30 18:33:06 +0800

    remove wrong files

--- tests/code_library/code_case/paddle_code/api_paddle_new_empty.py
+++
@@ -1,5 +0,0 @@
-import paddle
-
-print("#########################case1#########################")
-a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
-b = a.new_empty((3, 4), dtype=paddle.float64, requires_grad=True, pin_memory=True)
--- tests/code_library/code_case/paddle_code/api_paddle_new_tensor.py
+++
@@ -1,8 +0,0 @@
-import paddle
-
-print("#########################case1#########################")
-a = paddle.tensor([1, 2, 3])
-b = paddle.to_tensor(data=[4, 5, 6], dtype=paddle.float64, stop_gradient=not True)
-print("#########################case2#########################")
-a = paddle.tensor([1, 2, 3], dtype=paddle.int64)
-b = paddle.to_tensor(data=[4, 5, 6], dtype=a.dtype)
