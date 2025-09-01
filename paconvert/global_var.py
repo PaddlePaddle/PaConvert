7be269610b14793892324f05e741923be2316c06
Ayakouji <yuhongh@qq.com>
2025-09-01 15:41:58 +0800

    [API Compatibility] add some api (#619)
    * add torch.permute、torch.Tensor.permute
    
    * add test
    
    * fix
    
    ---------
    
    Co-authored-by: aquagull <hongyuh@qq.com>

--- paconvert/global_var.py
+++ paconvert/global_var.py
@@ -190,7 +190,17 @@ class GlobalManager:
         "torch.eye",
 
         # hongyu
-
+        "torch.permute",
+        "torch.Tensor.permute",
+        "torch.repeat_interleave",
+        "torch.Tensor.repeat_interleave",
+        "torch.Tensor.repeat",
+        "torch.maximum",
+        "torch.minimum",
+        "torch.topk",
+        "torch.sqrt",
+        "torch.amin",
+        "torch.amax",
 
         # linjun
         "torch.as_tensor", 
