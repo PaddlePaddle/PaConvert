b0e8391e9dacd2a864858b7b1982e2b22f71b632
hd9568 <2358440725@qq.com>
2025-09-02 07:14:44 +0000

    for consistency

--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_cast.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_cast.py
@@ -2,23 +2,26 @@ import paddle
 
 ############################## 相关utils函数，如下 ##############################
 
+
 def device2str(type=None, index=None, *, device=None):
     type = device if device else type
     if isinstance(type, int):
-        type = f'gpu:{type}'
+        type = f"gpu:{type}"
     elif isinstance(type, str):
-        if 'cuda' in type:
-            type = type.replace('cuda', 'gpu')
-        if 'cpu' in type:
-            type = 'cpu'
+        if "cuda" in type:
+            type = type.replace("cuda", "gpu")
+        if "cpu" in type:
+            type = "cpu"
         elif index is not None:
-            type = f'{type}:{index}'
+            type = f"{type}:{index}"
     elif isinstance(type, paddle.CPUPlace) or (type is None):
-        type = 'cpu'
+        type = "cpu"
     elif isinstance(type, paddle.CUDAPlace):
-        type = f'gpu:{type.get_device_id()}'
+        type = f"gpu:{type.get_device_id()}"
 
     return type
+
+
 ############################## 相关utils函数，如上 ##############################
 
 
@@ -41,7 +44,7 @@ print("#########################case7#########################")
 a = a.to(paddle.float16)
 print("#########################case8#########################")
 table = a
-b = a.to(table.place)
+b = a.to(table.device)
 print("#########################case9#########################")
 b = a.to(paddle.float32)
 print("#########################case10#########################")
