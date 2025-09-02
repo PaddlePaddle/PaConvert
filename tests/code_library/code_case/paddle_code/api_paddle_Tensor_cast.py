ed07718e2ebab44ffef4d7bc2b1be286183694e5
hd9568 <2358440725@qq.com>
2025-09-02 08:44:48 +0000

    modify api_paddle_Tensor_cast

--- tests/code_library/code_case/paddle_code/api_paddle_Tensor_cast.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Tensor_cast.py
@@ -2,26 +2,23 @@ import paddle
 
 ############################## 相关utils函数，如下 ##############################
 
-
 def device2str(type=None, index=None, *, device=None):
     type = device if device else type
     if isinstance(type, int):
-        type = f"gpu:{type}"
+        type = f'gpu:{type}'
     elif isinstance(type, str):
-        if "cuda" in type:
-            type = type.replace("cuda", "gpu")
-        if "cpu" in type:
-            type = "cpu"
+        if 'cuda' in type:
+            type = type.replace('cuda', 'gpu')
+        if 'cpu' in type:
+            type = 'cpu'
         elif index is not None:
-            type = f"{type}:{index}"
+            type = f'{type}:{index}'
     elif isinstance(type, paddle.CPUPlace) or (type is None):
-        type = "cpu"
+        type = 'cpu'
     elif isinstance(type, paddle.CUDAPlace):
-        type = f"gpu:{type.get_device_id()}"
+        type = f'gpu:{type.get_device_id()}'
 
     return type
-
-
 ############################## 相关utils函数，如上 ##############################
 
 
