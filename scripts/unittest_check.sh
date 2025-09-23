9f05cde102c4898ee1a62905c4ea46be16bf071d
zhouwei25 <zhouwei25@baidu.com>
2025-09-23 18:36:11 +0800

    Update CI torch version

--- scripts/unittest_check.sh
+++ scripts/unittest_check.sh
@@ -21,8 +21,11 @@ python -m pip install -U torch torchaudio torchvision --index-url https://downlo
 python -c "import torch; print('torch version information:' ,torch.__version__)"
 
 echo "Insalling develop cpu version paddle"
-#python -m pip uninstall -y paddlepaddle
-#python -m pip uninstall -y paddlepaddle-gpu
+python -m pip uninstall -y paddlepaddle
+python -m pip uninstall -y paddlepaddle-gpu
+
+#export http_proxy=
+#export https_proxy=
 python -m pip install --force-reinstall --no-deps -U --pre paddlepaddle -i https://www.paddlepaddle.org.cn/packages/nightly/cpu/
 python -c "import paddle; print('paddle version information:' , paddle.__version__); commit = paddle.__git_commit__;print('paddle commit information:' , commit)"
 
