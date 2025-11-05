13964efb75d054dc6f42dec6ac47dc7f90958d4b
AlAuAu <458134681@qq.com>
2025-11-05 11:57:58 +0800

    [API Compatibility] Add paddle.Tensor.clamp_ ，paddle.nn.functional.logsigmoid， paddle.functional.meshgrid， paddle.nn.init.calculate_fan_in_and_fan_out ，paddle.autocast

--- tests/test_nn_init_calculate_fan_in_and_fan_out.py
+++ tests/test_nn_init_calculate_fan_in_and_fan_out.py
@@ -17,12 +17,35 @@ import textwrap
 from apibase import APIBase
 
 obj = APIBase("torch.nn.init._calculate_fan_in_and_fan_out")
+
+
 def test_case_1():
     pytorch_code = textwrap.dedent(
         """
         import torch
-        x = torch.tensor([[[-1.3020, -0.1005, 0.5766, 0.6351, -0.8893, 0.0253, -0.1756, 1.2913],
-                            [-0.8833, -0.1369, -0.0168, -0.5409, -0.1511, -0.1240, -1.1870, -1.8816]]])
+        x = torch.randn(5, 5)
+        result = torch.nn.init._calculate_fan_in_and_fan_out(x)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_2():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.randn(5, 5, 5)
+        result = torch.nn.init._calculate_fan_in_and_fan_out(x)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_3():
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        x = torch.randn(5, 5, 5, 5)
         result = torch.nn.init._calculate_fan_in_and_fan_out(x)
         """
     )
