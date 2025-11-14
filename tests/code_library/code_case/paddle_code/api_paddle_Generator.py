39de1db5da9b95e65eda03c6d9a9a979a49c933c
fanxiangyu <fxyfxy777@163.com>
2025-11-14 13:26:25 +0800

    pre-commit

--- tests/code_library/code_case/paddle_code/api_paddle_Generator.py
+++ tests/code_library/code_case/paddle_code/api_paddle_Generator.py
@@ -2,15 +2,11 @@ import paddle
 
 print("#########################case1#########################")
 g_cpu = paddle.Generator()
-
 print("#########################case2#########################")
 g_cpu = paddle.Generator(device="cpu")
-
 print("#########################case3#########################")
 g_cpu = paddle.Generator("cpu")
-
 print("#########################case4#########################")
 g_cuda = paddle.Generator("cuda")
-
 print("#########################case5#########################")
 g_cuda = paddle.Generator(device="cuda")
