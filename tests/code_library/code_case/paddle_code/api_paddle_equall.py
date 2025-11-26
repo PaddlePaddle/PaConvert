07f8615cde2c6182b41ad4b6418c902f12ca4e0a
zhwesky2010 <1183042833@qq.com>
2025-11-26 17:32:39 +0800

    add some API to NO_NEED_CONVERT (#777)

--- tests/code_library/code_case/paddle_code/api_paddle_equall.py
+++ tests/code_library/code_case/paddle_code/api_paddle_equall.py
@@ -1,4 +1,4 @@
 import paddle
 
 print("#########################case1#########################")
-r = paddle.equal_all(x=paddle.tensor([1, 2]), y=paddle.tensor([1, 2])).item()
+r = paddle.compat.equal(paddle.tensor([1, 2]), paddle.tensor([1, 2]))
