937ad689c6626088fb6bd7e34c4e89c74c155d45
Wenfei (Charles) Qi <74716511+Manfredss@users.noreply.github.com>
2025-12-16 17:29:15 +0800

    Update NO_NEED_CONVERT_LIST for Pytorch API

--- paconvert/global_var.py
+++ paconvert/global_var.py
@@ -80,10 +80,10 @@ class GlobalManager:
     NO_NEED_CONVERT_LIST = [
         # Manfredss
         "torch.asin",
-        "torch.Tensor.gcd",
-        "torch.Tensor.gcd_",
-        "torch.Tensor.lcm",
-        "torch.Tensor.lcm_",
+        "torch.gcd",
+        "torch.gcd_",
+        "torch.lcm",
+        "torch.lcm_",
 
         # zhimin
         "torch.Tensor.bfloat16",
