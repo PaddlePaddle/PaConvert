088684bad496f25a542b5e194130ccead27fefe2
zhouwei25 <zhouwei25@baidu.com>
2025-12-31 03:45:03 +0800

    API Compatibility Edit by AI Agent(nextafter/angle/heaviside/asinh/reciprocal/square/tan)

--- tests/test_asinh.py
+++ tests/test_asinh.py
@@ -75,3 +75,65 @@ def test_case_5():
         """
     )
     obj.run(pytorch_code, ["out", "result"])
+
+
+def test_case_6():
+    """2D张量"""
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.tensor([[0.1606, -1.4267], [-1.0899, -1.0250]])
+        result = torch.asinh(a)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_7():
+    """3D张量"""
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.tensor([[[0.1, -1.0], [-1.5, 2.0]], [[3.0, -4.0], [5.0, -6.0]]])
+        result = torch.asinh(a)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_8():
+    """边界值 - 零值和大值"""
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.tensor([0.0, 1.0, -1.0, 10.0, -10.0])
+        result = torch.asinh(a)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_9():
+    """不同数据类型 - float64"""
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.tensor([0.1606, -1.4267, -1.0899], dtype=torch.float64)
+        result = torch.asinh(a)
+        """
+    )
+    obj.run(pytorch_code, ["result"])
+
+
+def test_case_10():
+    """梯度计算测试"""
+    pytorch_code = textwrap.dedent(
+        """
+        import torch
+        a = torch.tensor([0.5, 1.0, 2.0], requires_grad=True)
+        y = torch.asinh(a)
+        y.sum().backward()
+        a_grad = a.grad
+        """
+    )
+    obj.run(pytorch_code, ["y", "a_grad"], check_stop_gradient=False)
