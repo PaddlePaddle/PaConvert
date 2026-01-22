e296f0617b43245a5549891ec0b67aa419488d2b
fxyfxy777 <137464345+fxyfxy777@users.noreply.github.com>
2026-01-22 14:24:07 +0800

    fix device (#810)
    * fix device
    
    * rm cuda idx

--- tests/test_device.py
+++ tests/test_device.py
@@ -33,8 +33,6 @@ class DeviceAPIBase(APIBase):
         atol=0.0,
     ):
         pytorch_result = str(pytorch_result)
-        if pytorch_result == "cuda":
-            pytorch_result = "cuda:0"
         if "cpu:" in pytorch_result:
             pytorch_result = "cpu"
         assert pytorch_result == paddle_result
